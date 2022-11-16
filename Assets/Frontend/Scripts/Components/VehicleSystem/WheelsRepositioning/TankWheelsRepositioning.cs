73957e54fce2be336f6b1145416ce37d2ff24489
Black heart <52672318+szymonm992@users.noreply.github.com>
2022-11-16 11:06:13 +0100

    before wheels refactor

--- Assets/Frontend/Scripts/Components/VehicleSystem/WheelsRepositioning/TankWheelsRepositioning.cs
+++ Assets/Frontend/Scripts/Components/VehicleSystem/WheelsRepositioning/TankWheelsRepositioning.cs
@@ -192,7 +192,6 @@ namespace Frontend.Scripts.Components
                             dummy.Holder.localPosition = desiredDummyHolderPos;
                         }
                     }
-
                 }
             }
         }
