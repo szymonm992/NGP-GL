2997e60836a7037d8f83df7f48896c78bec166e0
Black heart <52672318+szymonm992@users.noreply.github.com>
2022-12-28 20:42:29 +0100

    unlocking input

--- Assets/Prototyping/Scripts/Components/PrototypingLevelManager.cs
+++ Assets/Prototyping/Scripts/Components/PrototypingLevelManager.cs
@@ -1,6 +1,7 @@
 using GLShared.General.Interfaces;
 using GLShared.General.Models;
 using GLShared.General.ScriptableObjects;
+using GLShared.General.Signals;
 using GLShared.Networking.Components;
 using Sfs2X.Entities;
 using System.Collections;
@@ -20,6 +21,10 @@ namespace Prototyping.Scripts.Components
         public void Initialize()
         {
             CreatePlayer("T-55", new Vector3(132.35f, 2f, 118.99f), new Vector3(0, 90f, 0));
+            signalBus.Fire(new PlayerSignals.OnAllPlayersInputLockUpdate()
+            {
+                LockPlayersInput = false,
+            });
         }
 
         private void CreatePlayer(string vehicleName, Vector3 spawnPosition, Vector3 spawnEulerAngles)
@@ -48,5 +53,6 @@ namespace Prototyping.Scripts.Components
             }
             return null;
         }
+
     }
 }
