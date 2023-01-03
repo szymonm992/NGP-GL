a5adfe2b1b02454469afe93c79e137262d7ecca3
Black heart <52672318+szymonm992@users.noreply.github.com>
2023-01-03 21:11:15 +0100

    scene changes

--- Assets/Prototyping/Scripts/Components/PrototypingLevelManager.cs
+++ Assets/Prototyping/Scripts/Components/PrototypingLevelManager.cs
@@ -59,7 +59,7 @@ namespace Prototyping.Scripts.Components
             yield return new WaitForSeconds(delay);
             for(int i=0; i< 20; i++)
             {
-                CreatePlayer("T-55", new Vector3(132.35f, 3.5f, 318.99f + (8 * i)), new Vector3(0, 90f, 0));
+                CreatePlayer("T-55", new Vector3(160f, 30.5f, 320f + (8 * i)), new Vector3(0, 90f, 0));
             }
            
             signalBus.Fire(new PlayerSignals.OnAllPlayersInputLockUpdate()
